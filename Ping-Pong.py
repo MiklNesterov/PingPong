5181b01b4420a5f61c4f828b93f39f43b57485f0
MiklNesterov <128513806+MiklNesterov@users.noreply.github.com>
2023-04-04 20:35:35 +0300

    Update Ping-Pong.py

--- Ping-Pong.py
+++ Ping-Pong.py
@@ -11,18 +11,42 @@ class Gamesprite(sprite.Sprite):
 
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
-   
+    
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 160:
+            self.rect.y += self.speed
+            
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 160:
+            self.rect.y += self.speed
+       
+
+racketka1 = Player ('', 30,200,4,50,150)
+racketka2 = Player ('', 520,200,4,50,150)
+Ping_ball = Gamesprite ('', 200,200,4,50,50)    
+    
 win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
 display.set_caption('Ужас, а не Хобар')
-
 background = Gamesprite('back.jpg', 0,0,0, win_width, win_height)
 
 game = True
 finish = False
 clock = time.Clock()
 FPS = 60
+
+font1 = font.Font(None,35)
+lose1 = font1.render('ПЕРВЫЙ ИГРОК ПРОИГРАЛ', True, (180,0,0))
+font2 = font.Font(None,35)
+lose3 = font2.render('ПЕРВЫЙ ИГРОК ПРОИГРАЛ', True, (180,0,0))
 
 while game:
     for e in event.get():
@@ -31,7 +55,29 @@ while game:
        
        
     if finish != True:
+        Ping_ball.rect.x += speed_x
+        Ping_ball.rect.y += speed_y
+        
         background.reset()
+        
+        racketka1.update_l()
+        racketka2.update_r()
+        
+        racketka1.reset()
+        racketka2.reset()
+        Ping_ball.reset()
+    
+    if Ping_ball.rect.y > win_height-50
+        or Ping_ball.rect.y < 0:
+            speed_y *= -1
+        
+    if Ping_ball.rect.x < 0:
+        finish = True
+        window.blit(lose1, (200,200))
+        
+    if Ping_ball.rect.x < 0:
+        finish = True
+        window.blit(lose2, (200,200))
     
     display.update()
     clock.tick(FPS)
